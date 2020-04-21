9b046e4512979f4df4425527088f558a05924e85
vortex7 <sean.wright.vortex@gmail.com>
2020-04-21 08:05:58 -0500

    modified: plug-ins/example-diagram.py

--- plug-ins/example-diagram.py
+++ plug-ins/example-diagram.py
@@ -5,18 +5,43 @@ from gimpfu import *
 def example_diagram(image, drawable):
     pdb.gimp_message("Example Diagram")
 
+    x = 200
+    y = 200
+    width = 350
+    height = 100
+    corner_radius_x = 50.0
+    corner_radius_y = 50.0
+    color = "#0E0E52" # space cadet
+    brush = "1. Pixel"
+    brush_size = 4
+
+    pdb.gimp_message("Good so far...")
+
     # Add first box
-    pdb.gimp_image_select_round_rectangle(image, 0, 200, 200, 350, 100, 50, 50)
-    pdb.gimp_context_set_foreground( ("#0E0E52") ) # space cadet
-    pdb.gimp_context_set_brush("1. Pixel")
-    pdb.gimp_context_set_brush_size(4)
-    pdb.gimp_context_set_stroke_method(1)
+    pdb.gimp_image_select_round_rectangle(image, CHANNEL_OP_ADD, x, y, width, height, corner_radius_x, corner_radius_y)
+    pdb.gimp_context_set_foreground(color)
+    pdb.gimp_context_set_brush(brush)
+    pdb.gimp_context_set_brush_size(brush_size)
+    pdb.gimp_context_set_stroke_method(STROKE_PAINT_METHOD)
     pdb.gimp_edit_stroke(drawable)
 
-    pdb.gimp_context_set_foreground( ("#000000") ) # black
-    font = "Serif"
-    text_layer = pdb.gimp_text_layer_new(image, "test", font, 40.0, 0)
-    pdb.gimp_image_insert_layer(image, text_layer, None, 0)
+    color = "#000000" # black
+    pdb.gimp_context_set_foreground(color)
+
+
+    text = "Test"
+    border = 0
+    antialias = True
+    size = 40.0
+    size_type = PIXELS
+    fontname = "Verdana"
+
+    text_width, text_height, text_ascent, text_descent = pdb.gimp_text_get_extents_fontname(text, size, size_type, fontname)
+
+    x = x + ( ( width / 2 ) - ( text_width / 2 ) )
+    y = y + ( ( height / 2 ) - ( text_height / 2 ) )
+
+    text_layer = pdb.gimp_text_fontname(image, None, x, y, text, border, antialias, size, size_type, fontname)
 
     # select none
     pdb.gimp_selection_none(image)
@@ -30,7 +55,7 @@ register(
     "ZDG",
     "2020",
     "<Image>/File/Example Diagram",
-    "RGB",
+    "*",
     [],
     [],
     example_diagram)
